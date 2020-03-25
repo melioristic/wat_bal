80e69ca2168db9c7a5ae522cc530fe52a77c290b
Mohit Anand <itsmohitanand@gmail.com>
2020-03-25 13:02:28 +0530

    autopep8

--- run_sim.py
+++ run_sim.py
@@ -3,37 +3,37 @@ from utils import read_parameters, GOF
 import numpy as np
 from plots import OutputPlot
 
-file_name = "LATTERBACHmeteo.txt" 
-var = VariablePrep(file_name, cal_factor = 0, cal_data = False, val_data = True)
+file_name = "LATTERBACHmeteo.txt"
+var = VariablePrep(file_name, cal_factor=0, cal_data=False, val_data=True)
 
 # Manually enter the parameter values after seeing the plot
 
-param = {"s_max":39.4039, 
-            "rg":17.9764,
-            "k":0.8265,
-            "fr":0.0688
-            }
+param = {"s_max": 39.4039,
+         "rg": 17.9764,
+         "k": 0.8265,
+         "fr": 0.0688
+         }
 
 num_time_steps = var.j_day.shape[0]
 
-output = np.zeros((var.j_day.shape[0],15))
+output = np.zeros((var.j_day.shape[0], 15))
 
 for step in range(num_time_steps):
     variables = {
-        "j_day":var.j_day[step],
-        "temp":var.t_av[step],
-        "temp_max":var.t_max_av[step],
-        "temp_min":var.t_min_av[step],
-        "precip":var.precip[step]
+        "j_day": var.j_day[step],
+        "temp": var.t_av[step],
+        "temp_max": var.t_max_av[step],
+        "temp_min": var.t_min_av[step],
+        "precip": var.precip[step]
     }
     if step == 0:
-        model = ConceptualWatbalModel(parameters = param)
-        output[step] = model.simulate(variables = variables)
+        model = ConceptualWatbalModel(parameters=param)
+        output[step] = model.simulate(variables=variables)
     else:
         output[step] = model.simulate(variables=variables)
 
 
-gof_class = GOF(sim = output[:,0], obs = var.q)
+gof_class = GOF(sim=output[:, 0], obs=var.q)
 gof_class.save_gof()
 
 
--- utils.py
+++ utils.py
@@ -3,26 +3,28 @@ import h5py
 import numpy as np
 
 
-def read_data(filename = "HONDRICHmeteo.txt"):
+def read_data(filename="HONDRICHmeteo.txt"):
 
     data = pd.read_csv("data/"+filename, delimiter="\t", skiprows=1)
 
     j_day = data.day.values
     precip = data.P_mix.values
-    t_max = data.T_max.values    
-    t_min = data.T_min.values    
+    t_max = data.T_max.values
+    t_min = data.T_min.values
     q = data.Q.values
 
     return j_day, precip, t_max, t_min, q
 
+
 def read_parameters():
-    with h5py.File("parameters.h5","r") as f:
+    with h5py.File("parameters.h5", "r") as f:
         nruns = len(f.keys())-1
-        param_arr = np.zeros((nruns,4))
+        param_arr = np.zeros((nruns, 4))
         for run in range(nruns):
-            param_arr[run,:] = f.get("params_"+str(run))[:]
+            param_arr[run, :] = f.get("params_"+str(run))[:]
 
     return param_arr
+
 
 class GOF(object):
     def __init__(self, sim, obs):
@@ -31,16 +33,16 @@ class GOF(object):
 
     def mean_error(self):
         return np.mean(self.obs-self.sim)
-    
+
     def percent_bias(self):
         return 100*(np.mean(self.sim)-np.mean(self.obs))/np.mean(self.obs)
-    
+
     def mean_absolute_error(self):
         return np.mean(np.abs(self.obs-self.sim))
 
     def root_mean_square_error(self):
         return np.sqrt(np.mean(np.square(self.obs-self.sim)))
-    
+
     def nash_shutcliffe_efficiency(self):
         numerator = np.sum(np.square(self.obs-self.sim))
         denominator = np.sum(np.square(self.obs-np.mean(self.obs)))
@@ -48,7 +50,8 @@ class GOF(object):
 
     def index_of_agreement(self):
         numerator = np.sum(np.square(self.obs-self.sim))
-        denominator = np.sum(np.square((np.abs(self.sim-np.mean(self.sim)))+(np.abs(self.obs-np.mean(self.obs)))))
+        denominator = np.sum(np.square(
+            (np.abs(self.sim-np.mean(self.sim)))+(np.abs(self.obs-np.mean(self.obs)))))
 
         return 1-numerator/denominator
 
@@ -60,12 +63,13 @@ class GOF(object):
         nse = self.nash_shutcliffe_efficiency()
         ioa = self.index_of_agreement()
 
-        data = np.array([me, pb, mae, rmse, nse, ioa]).reshape(1,6)
-      
-        col_names = ["Mean Error", "Percent Bias","Mean Absolute Error", "Root Mean Square Error", "Nash-Shutcliffe-Efficiency", "Index of Agreement"]
+        data = np.array([me, pb, mae, rmse, nse, ioa]).reshape(1, 6)
+
+        col_names = ["Mean Error", "Percent Bias", "Mean Absolute Error",
+                     "Root Mean Square Error", "Nash-Shutcliffe-Efficiency", "Index of Agreement"]
 
         for i in range(len(col_names)):
             print(f"{col_names[i]}  : {np.round(data[0,i],3)}")
- 
-        gof = pd.DataFrame(data = data, columns = col_names)
+
+        gof = pd.DataFrame(data=data, columns=col_names)
         gof.to_csv("gof.csv")
